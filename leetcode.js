a3f3506c7bc39f59732dcccc5e313a6b1e10740f
unknown <shantilal.kachhawa@supersourcing.com>
2024-07-01 19:04:23 +0530

    leetcode second prbl,

---
+++ leetcode.js
@@ -0,0 +1,151 @@
+// merge to sorted array same or  not
+
+// const arr1 = [1, 2, 3,10];
+// const arr2 = [2, 4, 6,7];
+// const arr3 = []
+
+// let i=0;
+// let j=0;
+// let k=0;
+
+// while(i < arr1.length && j < arr2.length){
+//     if(arr1[i] < arr2[j]){
+//         arr3[k] = arr1[i];
+//         i++;
+//     }else{
+//         arr3[k] = arr2[j];
+//         j++;
+//     }
+//     k++;
+// }
+// while(i < arr1.length){
+//     arr3[k] = arr1[i];
+//     i++;
+//     k++;
+// }
+// while(j < arr2.length){
+//     arr3[k] = arr2[j];
+//     j++;
+//     k++;
+
+// }
+// console.log(arr3,'arr3');
+
+
+// ----------------------------------------------------------------
+    // two index sum
+
+    // Example 1:
+
+    // Input: nums = [2,7,11,15], target = 9
+    // Output: [0,1]
+    // Example 2:
+    // Input: nums = [3,2,4], target = 6
+    // Output: [1,2]
+    // Example 3:
+    // Input: nums = [3,3], target = 6
+    // Output: [0,1]
+    // let nums = [1,0,1,3,3]
+    // let target = 6;
+
+    // for(let  i =0; i < nums.length; i++) {
+    //     for(let j = i+1; j < nums.length; j++){
+    //         if(nums[i] + nums[j] === target){
+    //             console.log(i,j);
+    //         }
+    //     }
+
+    // }
+
+    // ----------------------------------------------------------------
+    // 2. Add Two Numbers
+
+
+    // let  l1 = [2,8,3,2,2,0]
+    // let  l2 = [5,6,4]
+    // let  l3 = []
+    // let  carryValue =0;
+    // let k =0;
+    // let firstArr=l1.length
+    // let secArr=l2.length
+ 
+    // if( firstArr < secArr){
+    //     for(let i = firstArr;i < secArr;i++ ){
+    //         l1[i]= 0 ;
+    //     }
+        
+    // }else{
+    //    for(let j = secArr;j < firstArr;j++ ){
+    //         l2[j]=0;
+    //     }  
+    // }
+    //   console.log(l1)
+    //   console.log(l2)
+ 
+    // for (let i = 0; i < l1.length; i++) {
+    //  for(let j = i; j <= i; j++) {
+    //      let sum =  l1[i] + l2[j]+ carryValue;
+    //       if (sum >= 10) {
+    //          carryValue = Math.floor(sum / 10);
+    //          sum = sum % 10; 
+    //      } else {
+    //          carryValue = 0;
+    //      }
+    //      l3[k] =sum
+    //      k ++;
+ 
+    //  }
+ 
+    // }
+    // console.log(l3)
+
+    // Optimsie
+    let  l1 = [2,8,3,2,2,67]
+    let  l2 = [5,6,4,8,6,44]
+    let i= 0;
+    let str='';
+    let carryValue =0;
+    let firstArr=l1.length
+    let secArr=l2.length
+    let treveseLength = 0;
+
+
+    if(firstArr > secArr) {
+        treveseLength=firstArr;
+    }else{
+        treveseLength=secArr;
+    }
+    while(i < treveseLength ){
+    let sum = (l1[i] ||0) + (l2[i]||0) + carryValue;
+    if(sum >=10){
+        carryValue=Math.floor(sum/10);
+        sum=sum%10
+    }else{
+        carryValue=0;
+    }
+    str+=sum;
+    // 67 + 67 =134
+//    when last carry value is greater then carryValue
+    if(treveseLength-1 === i && carryValue > 0){
+        if(carryValue < 9){
+            str+=carryValue;
+            carryValue=0;
+        }else{
+            while(carryValue!=0){
+                str+=carryValue%10;
+                carryValue=Math.floor(carryValue/10);
+            }
+        }
+        // console.log(carryValue,'carryValue');
+       
+    }
+    i++;
+   
+
+    }
+    console.log(str);
+    
+
+
+    
+
