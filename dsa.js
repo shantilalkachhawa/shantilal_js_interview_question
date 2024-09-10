ed4865231d1e7ae46d5472abb790fc36f7c01d75
unknown <shantilal.kachhawa@supersourcing.com>
2024-09-10 18:58:59 +0530

    pattern que

---
+++ dsa.js
@@ -0,0 +1,71 @@
+// Apna college DSA Lectures
+
+// solid Recatanble
+
+// for (let row =1; row<= 5; row++){
+// let output=''
+//     for (let col=1;col<=5; col++){
+//         // console.log(col);
+//         output+=col
+
+//     }
+//     console.log(output);
+    
+// }
+
+// Holo rectangles
+// ******
+// *    *
+// *    *
+// *    *
+// *    *
+// ******
+
+// let row = 5;
+// let col = 5;
+// for(let i=1;i<=row; i++) {
+//     let output='';
+//     for (let j=1;j<=5;j++) {
+//         if(i==1 || j ==1 || i == row || j== col){
+//             output+='*';
+//         }else{
+//             output+=' ';
+//         } 
+//     }
+// console.log(output);
+// }
+
+// print halfe peramid
+// *
+// **
+// ***
+// ****
+
+// for(let i = 0; i < 4; i++){
+//     let output = '';
+//     for(let j=0;j<=i; j++){
+//         // output+=i
+//         output=output +i;
+//     }
+//     console.log(output);
+
+// }
+// Invert half permid 
+// *****
+// ****
+// ***
+// **
+// *
+// let row= 5
+// for(let i=row;i>=1;i--){
+// let output= ''
+//     for(let j=1;j<=i;j++){
+//        output+='*'
+//     }
+//     console.log(output);
+
+// }
+
+// inverted half peramid 180
+
+// *****
