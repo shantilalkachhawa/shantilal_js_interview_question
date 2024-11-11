845e5de14ff0ae4b9321810511d3baa1b7486dd2
shantilal <shantilal.kachhawa@avista.com>
2024-11-11 19:36:44 +0530

    learn gen ai

---
+++ generative_ai.js
@@ -0,0 +1,7 @@
+// ineron web dashboard
+
+// Genrative AI
+
+// Ai->ML->DL(deep l) ->Gai (subset of DL)
+// universal lang mode-fine tuning for text genration (while using chat gpt,we can get excact result through of fine tuning)
+// Gpt- doing ready for  reinforcment learning platform (human read in 20yr appro 45-50 tb data-all internate date  )
