522be212aa91699a2bc655dc383947aa432bbfc9
shantilal kachhawa <kachhawashantilal@gmail.com>
2025-01-20 10:20:16 +0530

    lect 30

--- 100.js
+++ 100.js
@@ -704,30 +704,44 @@
 //     return arr;
 
 // }
-// console.log(sortColor([2,0,2,1,1,0]))
-// console.log(sortColor([2,0,1]))
+// console.log(sortColor([2,0,2,1,1,0])) //[ 0, 0, 1, 1, 2, 2 ]
+// console.log(sortColor([2,0,1])) //[ 0, 1, 2 ]
 
 
 // Maximum Subarray | Day 29  OR Kadane’s Algorithm
 // Maximum Subarray using O(n^2) approach
-function maxSubArr(arr){
-    let maxSum = -Infinity;
-    for(let i = 0; i < arr.length; i++){
-        let currSum = 0;
-        for(let j = i ; j < arr.length; j++){
-            currSum +=arr[j];
-            if(currSum > maxSum){
-                maxSum = currSum;
-            }
-        }
-    }
-    return maxSum;
-}
+// function maxSubArr(arr){
+//     let maxSum = -Infinity;
+//     for(let i = 0; i < arr.length; i++){
+//         let currSum = 0;
+//         for(let j = i ; j < arr.length; j++){
+//             currSum +=arr[j];
+//             if(currSum > maxSum){
+//                 maxSum = currSum;
+//             }
+//         }
+//     }
+//     return maxSum;
+// }
 // Maximum Subarray using optomise approach
-function maxSubArr(arr){
+// function maxSubArr(arr){
+//     let currSum=arr[0];
+//     let max=arr[0];
+//     for(let i = 1; i < arr.length ; i++){
+//         if((currSum + arr[i]) > arr[i]){
+//             currSum += arr[i];
+//         }else{
+//             currSum = arr[i];
+//         }
+//         max = Math.max(max,currSum);
+//     }
+//     return max;
+
   
-}
-console.log(maxSubArr([-2,1,-3,4,-1,2,1,-5,4]))
-console.log(maxSubArr([1]))
-console.log(maxSubArr([5,4,-1,7,8]))
+// }
+// console.log(maxSubArr([-2,1,-3,4,-1,2,1,-5,4]))
+// console.log(maxSubArr([1]))
+// console.log(maxSubArr([5,4,-1,7,8]))
+
+//  Trapping Rain Water | Day 30 
 
