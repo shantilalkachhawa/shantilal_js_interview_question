6920616868bba0bb97e0ed27d8e39187d1859d60
shantilal <kachhawashantilal001@gmail.com>
2025-04-30 11:15:52 +0530

    some file

--- coding.js
+++ coding.js
@@ -1,5 +1,136 @@
 //Create BY Shantilal kachhawa-------JS-------//
 
+
+
+// My name is Shantilal Kachhawa, and I have over 3 years of experience in Frontend development. I have worked on a variety of projects across different domains and have strong skills in React, Angular, Redux, TypeScript, JavaScript, and responsive design.
+
+
+// in famihives
+// - Famehives is a family organization management web app. 
+// - Users can signup/signin and add their family memebers.
+// - Users can visualize their family in a tree structure
+// - I have built a custom solution to make family tree, and developed my own npm package and published it.
+// - I have also developed a customer management portal for the system, where admin can see some analytics like users information, control master content, add/update terms & condition / policy
+// preserve your family history with our family tree management
+// Implemented push notification while using one signal and real-time chat with socket,
+// manage RBAC role-based access control user ,
+// added switch laguage functionality ,
+// also create user management portal ,
+
+
+// Public JOb
+// In this project, users can view jobs based on their role and easily apply for those positions through the web application.
+//  Additionally, ATS (Applicant Tracking System) features are implemented to manage the hiring process. For example, if a job requires 1 or 2 candidates but receives multiple resumes, AI is used to compare the resumes with the job description (JD).
+//  The system then returns a resume score, highlighting both the candidate's strong and weak areas, aiding in the decision-making process.
+
+
+
+
+
+
+
+//Js Interview Question
+// 1 accenture
+// 'use strict'
+//  {
+//     function output(){
+//         return 'is ruturn'
+//     }
+//  }
+
+//  output()
+//  console.log(output());
+//  yes called the function cuz this is fuctional scope but when use use-strict then fun are converted to block scope.
+
+
+//2 spotify
+
+// function output(a,b){
+//     return 'output length is'
+
+// }
+// function output1(a,b=0,c){
+//     return 'output1 length is'
+
+// }
+// function output2(a,b=0,...rest){
+//     return 'output2 length is'
+
+// }
+// function output3(a,b,...rest){
+//     return 'output3 length is'
+
+// }
+// console.log(output.length,'output');
+// console.log(output1.length,'output1');
+// console.log(output2.length,'output2');
+// console.log(output3.length,'output3');
+
+
+// 3 OLX
+
+// function result(){
+//     this.name = 'shanti'
+//     return this
+// }
+
+
+// const result = ()=>{
+//     this.name = 'shanti'
+// return this 
+// }
+
+// const output =new result()
+// console.log(output);
+// Normal fun are return value but arraow fun are return error is constructor cuz arrow not supprot this key word.
+
+
+
+// 5 array 
+// var output = [1,2,3];
+// output[6]=10
+
+// console.log(output);
+// [ 1, 2, 3, <3 empty items>, 10 ] o/p is create are empty item space
+
+
+// 5 array 
+// var output = [1,2,3];
+// output[6]=10
+
+// console.log(output);
+// [ 1, 2, 3, <3 empty items>, 10 ] o/p is create are empty item space
+
+
+// customs slider 
+// var array =['s','h','a','n']
+// let lenght = array.length
+// var index = 0;
+
+// function slider(){
+//     console.log(array[index]);
+//     index = (index + 1)% lenght;
+// }
+// setInterval(slider,1000)
+
+// 
+// function show(a){
+
+//     console.log(a);
+//     if(a >0){
+//         show(a-1);
+//     }
+//     console.log(a);
+// }
+// show(5)
+
+// 
+// let a= [1,2,3,4,5];
+// a[a.length-1]++;
+
+// console.log(a);
+// [1,2,3,4,6]
+
 // conver to the number 
 // " 33"=>33
 // " 33aaa"=>NaN
@@ -547,31 +678,31 @@
 // }
 
 // console.log(lengthOfLongestSubstring("abcabcbb"));
-    console.log(lengthOfLongestSubstring("bbbbbb"));
+// console.log(lengthOfLongestSubstring("bbbbbb"));
 // console.log(lengthOfLongestSubstring("abcdef"));
 // console.log(lengthOfLongestSubstring(""));
 // console.log(heh("GEEKSFORGEEKS"));
 // console.log(heh("pwwkew"));
 // console.log(lengthOfLongestSubstring("GEEKSFORGEEKS"));
 
-    function lengthOfLongestSubstring(str){
-        let length=0;
-        let maxStr =''
+    // function lengthOfLongestSubstring(str){
+    //     let length=0;
+    //     let maxStr =''
 
-        for (let i = 0; i < str.length; i++) {
-            for (let j = i + 1; j <= str.length; j++) {
-                let subs = str.substring(i, j);
-                let rev = subs.split('').reverse().join('');
-                if (subs === rev) {
-                    if (subs.length > length) {
-                        length = subs.length;
-                        maxStr = rev;  // Update maxStr here
-                    }
-                }
-            }
-        }
-        return {length ,maxStr}
-    }
+    //     for (let i = 0; i < str.length; i++) {
+    //         for (let j = i + 1; j <= str.length; j++) {
+    //             let subs = str.substring(i, j);
+    //             let rev = subs.split('').reverse().join('');
+    //             if (subs === rev) {
+    //                 if (subs.length > length) {
+    //                     length = subs.length;
+    //                     maxStr = rev;  // Update maxStr here
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return {length ,maxStr}
+    // }
 
 
 
@@ -581,4 +712,340 @@
     // Emits latest value to new subscribers
     // Initial value required
 // Subject
-    // Subject, which does not retain the last emitted value
+// Subject, which does not retain the last emitted value
+// const array = [1,2,[3,4,[5,6]]]
+
+// function rec(arr){
+//     let res =[];
+//    for(let item of arr){
+//        console.log(res,'res')
+//        if(Array.isArray(item)){
+//            res = res.concat(rec(item));
+//            console.log(res,'iffff')
+           
+//        }else{
+//            res.push(item);
+//        }
+//    }
+//    return res;
+    
+// }
+// console.log(rec(array))
+
+
+        // selection sort Algorithm
+        // let data=[20,12,53,3]
+        // Assending sort
+        // function selectionSort(items){
+        //     let n= items.length;
+        //     let minValue;
+        //     for(let i=0;i<n; i++){
+        //         minValue=i;
+        //         for(let j=i+1;j<n;j++){
+        //             // 12 < 20
+        //             if( items[j] < items[minValue] ){
+        //                 minValue = j
+        //             }
+        //         }
+        //         let temp= items[i];
+        //         items[i]= items[minValue];
+        //         items[minValue]=temp
+        //     }
+        // }
+        // function selectionSort(items){
+        //     let n= items.length;
+        //     let minValue;
+        //     for(let i=0;i<n; i++){
+        //         minValue=i;
+        //         for(let j=i+1;j<n;j++){
+        //             // 12 > 20
+        //             if( items[j] > items[minValue] ){
+        //                 minValue = j
+        //             }
+        //         }
+        //         let temp= items[i];
+        //         items[i]= items[minValue];
+        //         items[minValue]=temp
+        //     }
+        // }
+        // selectionSort(data);
+        // console.log(data,'data');
+
+        // Insertion sort
+        // let data=[20,11,12,5,6]
+        // function insertionSort(items){
+        //     let n= items.length;
+        //     let currentValue,j;
+        //     for(let i=1; i<n; i++){
+        //         currentValue= items[i];
+        //         j=i-1;
+        //         while( j >=0, items[j] > currentValue){
+        //             items[j+1] =items[j]
+        //             j--;
+        //         }
+        //         items[j+1]=currentValue;
+        //     }
+        // }
+        // insertionSort(data);
+        // console.log(data,'data');
+        
+
+
+   
+//    Longest Substring Without Repeating Characters
+
+// function longestSubstringWithoutRepeatingCharacters(s) {
+//     let maxLength = 0;
+//     let start = 0;
+//     let charIndexMap = {};
+
+//     for (let end = 0; end < s.length; end++) {
+//         let char = s[end];
+        
+//         if (charIndexMap[char] !== undefined && charIndexMap[char] >= start) {
+//             start = charIndexMap[char] + 1;
+//         }
+
+//         charIndexMap[char] = end;
+//         maxLength = Math.max(maxLength, end - start + 1);
+//     }
+
+//     return maxLength;
+// }
+// function lengthOfLongestSubstring(str) {
+//     let maxLength = 0;
+//     let start = 0 ;
+//     let end = 0 ;
+//     let substr = new Set();
+//     while(end < str.length){
+//         if(!substr.has((str[end]))){
+//             substr.add(str[end])
+//             end++;
+//             maxLength = Math.max(maxLength, end - start);
+
+//         }else{
+//          substr.delete(str[start]);
+//             start++;
+//         }
+//     }
+//     return maxLength;
+// }
+
+// console.log(lengthOfLongestSubstring("abcabcbb"));
+// console.log(lengthOfLongestSubstring("bbbbb"));
+// console.log(lengthOfLongestSubstring("abcdef"));
+// console.log(lengthOfLongestSubstring(""));
+// console.log(heh("GEEKSFORGEEKS"));
+// console.log(heh("pwwkew"));
+// console.log(lengthOfLongestSubstring("GEEKSFORGEEKS"));
+
+// Median of Two Sorted Arrays
+
+// let nums1 = [1,2];
+// let nums2 = [3,4];
+// let mergedArray=[]
+// i=0;
+// j=0;
+// k=0;
+// while(i < nums1.length && j < nums2.length){
+//     if(nums1[i] < nums2[j]){
+//         mergedArray[k]=nums1[i]
+//         i++
+//     }else{
+//         mergedArray[k]=nums2[j]
+//         j++;
+//     }
+//     k++
+// }
+// while(i < nums1.length){
+//     mergedArray[k]=nums1[i]
+//     k++;
+//     i++;
+// }
+// while(j < nums2.length){
+//     mergedArray[k]=nums2[j]
+//     k++;
+//     j++;
+// }
+// let median;
+// if(mergedArray){
+//     let mid = Math.floor(mergedArray.length/2);
+//     if(mergedArray.length % 2===0){
+//         median =(mergedArray[mid-1] + mergedArray[mid])/2
+//     }else{
+//         median= mergedArray[mid];
+//     }
+// }
+// console.log(median);
+
+// Longest Palindromic Substring
+//  s = "babad"
+// ----------------------------------------------------------------
+
+
+// Find enven value s form array, Make double og every eleent and find array length
+// function evenNumber(arr){
+//     let newarr = [];
+//     let j=0;
+
+//     for(let i=0; i <arr.length; i++){
+//         if(arr[i]%2 === 0){
+//             newarr[j] = arr[i]*2;
+//         j++;
+//         }
+
+//     }
+//     return {newarr,j};
+// }
+// let a =[6,5.10,13,14,16,17]
+// console.log(evenNumber(a));
+
+// Check string are anagram
+// check- both length are same ,repeat chart
+
+//  function isAnagram(str1, str2) {
+//     if(str1.length !== str2.length){
+//         return false;
+//     }
+//     let charCount1 = {};
+//     for(let i = 0; i < str1.length; i++){
+//     charCount1[str1[i]] = (charCount1[str1[i]] || 0) + 1;
+//     }
+//     for(let i = 0; i < str2.length; i++){
+//     if(!charCount1[str2[i]]){
+//     return   false
+//     }
+//     charCount1[str2[i]]--;
+// if(charCount[str[i]]){
+//             charCount[str[i]]++;
+//         } else{
+//             charCount[str[i]] = 1;
+//         }
+// }
+// return true
+// }
+// let str1 ='cat';
+// let str2 ='tsc'
+// console.log(isAnagram(str1,str2));
+
+// Check string is Palendrome
+// function checkPalendromeString(str){
+    // let reversedStr = str.split('').reverse().join('');
+    // if(str === reversedStr){ 
+    //     return true;
+    // }else{
+    //     return false;
+    // }
+
+    // let index = 0;
+    // let j = str.length - 1;
+    // for (let index = 0; index < str.length; index++) {
+    //     if(str[index]!==str[j]){
+    //         return false;
+    //     }
+    //     j--;
+    // }
+    // return true
+
+    // let start=0;
+    // let end=str.length-1;
+    // let result = true
+    // while(start < end){
+    //     if(str[start]!== str[end]){
+    //         result = false;
+    //         break;
+    //     }
+    //     start++;
+    //     end--;
+    // }
+    // return result;
+// }
+// let str='level'
+// console.log(checkPalendromeString(str));
+
+// Maximum occuring characters in a string
+// function maxOccurringCharacter(str) {
+//     let charCount = {};
+//     let maxCount = 0;
+//     let maxChar = '';
+
+//     for (let i = 0; i < str.length; i++) {
+//         let char = str[i];
+//         if (charCount[char]) {
+//             charCount[char]++;
+//         } else {
+//             charCount[char] = 1;
+//         }
+//         if (charCount[char] > maxCount) {
+//             maxCount = charCount[char];
+//             maxChar = char;
+//         }
+//     }
+
+//     return {maxChar,maxCount};
+// }
+
+// let srt='hello world'
+// console.log(maxOccurringCharacter(srt));
+
+// Object (Hash Table) DS in js
+
+// longestPalindrome----------------------------------------------------------------
+
+// function longestPalindrome(s) {
+//     if (s.length < 1) return "";
+
+//     let start = 0, end = 0;
+// debugger;
+//     const expandAroundCenter = (s, left, right) => {
+//         while (left >= 0 && right < s.length && s[left] === s[right]) {
+//             left--;
+//             right++;
+//         }
+//         return right - left - 1; // Length of the palindrome
+//     };
+
+//     for (let i = 0; i < s.length; i++) {
+//         let len1 = expandAroundCenter(s, i, i); // Odd length palindromes
+//         let len2 = expandAroundCenter(s, i, i + 1); // Even length palindromes
+//         let len = Math.max(len1, len2);
+//         if (len > end - start) {
+//             start = i - Math.floor((len - 1) / 2);
+//             end = i + Math.floor(len / 2);
+//         }
+//     }
+//     return s.substring(start, end + 1);
+// }
+
+// // Example usage:
+// // const inputString = "babad";
+// const inputString = "cbbd";
+// const longestPalindromicSubstring = longestPalindrome(inputString);
+// console.log(longestPalindromicSubstring); // Output: "bab" or "aba"
+
+// function longestPallindrome(str) {
+//     let string = str.toLowerCase();
+//     let pallindrome = '';
+    
+//     function strReverser(start, end) {
+//         const rev = string.substr(start, end).split('').reverse().join('');
+//         return rev;
+//     }
+    
+//     for(let i=0; i<=string.length; i++) {
+//         for(let j=i; j<=string.length; j++) {
+//             if(string.substr(i,j) == strReverser(i,j)) {
+//                 if(string.substr(i,j).length > pallindrome.length) {
+//                     pallindrome = string.substr(i,j);
+//                 }
+//             }
+//         }
+//     }
+    
+//     if(pallindrome != null) {
+//         return pallindrome;
+//     }
+// }
+// let result1 = longestPallindrome('My mom is called annnna')
+// let result2 = longestPallindrome('My dad is a racecar athelete')
+
