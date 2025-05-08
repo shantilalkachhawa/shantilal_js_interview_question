4ca6f2f9a7d59632b3bdf781159ecf43a15b901e
shantilal kachhawa <kachhawashantilal@gmail.com>
2025-05-08 08:55:53 +0530

    changes

--- 100.js
+++ 100.js
@@ -2215,3 +2215,48 @@ function increTripletSub(arr){
 
 
 // Spiral Matrix II | Day 58
+
+function generateMatrix(n) {
+    const matrix = Array.from({ length: n }, () => Array(n).fill(0));
+    let num = 1;
+    let top = 0;
+    let bottom = n - 1;
+    let left = 0;
+    let right = n - 1;
+
+    while (top <= bottom && left <= right) {
+        // Left ➔ Right
+        for (let i = left; i <= right; i++) {
+            matrix[top][i] = num++;
+        }
+        top++;
+
+        // Top ➔ Bottom
+        for (let i = top; i <= bottom; i++) {
+            matrix[i][right] = num++;
+        }
+        right--;
+
+        // Right ➔ Left
+        if (top <= bottom) {
+            for (let i = right; i >= left; i--) {
+                matrix[bottom][i] = num++;
+            }
+            bottom--;
+        }
+
+        // Bottom ➔ Top
+        if (left <= right) {
+            for (let i = bottom; i >= top; i--) {
+                matrix[i][left] = num++;
+            }
+            left++;
+        }
+    }
+
+    return matrix;
+}
+
+// Example usage:
+console.log(generateMatrix(3));
+
