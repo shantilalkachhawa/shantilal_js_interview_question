bb5dcdf30eccdc7c2650059fe7ae3f587d006d91
shantilal kachhawa <kachhawashantilal@gmail.com>
2025-05-09 10:25:15 +0530

    search matrix

--- 100.js
+++ 100.js
@@ -2261,33 +2261,33 @@ function generateMatrix(n) {
 console.log(generateMatrix(3));
 
 
-// function searchMatrix(matrix, target) {
-//     if (!matrix.length || !matrix[0].length) return false;
-//     const rows = matrix.length;
-//     const cols = matrix[0].length;
-//     let left = 0;
-//     let right = rows * cols - 1;
-//     while (left <= right) {
-//         const mid = Math.floor((left + right) / 2);
-//         const midValue = matrix[Math.floor(mid / cols)][mid % cols];
-//         if (midValue === target) {
-//             return true;
-//         } else if (midValue < target) {
-//             left = mid + 1;
-//         } else {
-//             right = mid - 1;
-//         }
-//     }
-//     return false;
-// }
-// const mat = [
-//  [1, 3, 5, 7],
-//  [10, 11, 16, 20],
-//  [23, 30, 34, 60]
-// ];
+function searchMatrix(matrix, target) {
+    if (!matrix.length || !matrix[0].length) return false;
+    const rows = matrix.length;
+    const cols = matrix[0].length;
+    let left = 0;
+    let right = rows * cols - 1;
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        const midValue = matrix[Math.floor(mid / cols)][mid % cols];
+        if (midValue === target) {
+            return true;
+        } else if (midValue < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return false;
+}
+const mat = [
+ [1, 3, 5, 7],
+ [10, 11, 16, 20],
+ [23, 30, 34, 60]
+];
 
-// console.log(searchMatrix(mat, 3));  // true
-// console.log(searchMatrix(mat, 13)); // false
+console.log(searchMatrix(mat, 3));  // true
+console.log(searchMatrix(mat, 13)); // false
 
 
 function searchMatrix(matrix, target) {
